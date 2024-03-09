4234db5e9db303aa921adf66e83f5b926158fb57
Johnny Fong <johnnyfong@live.com>
2024-03-09 17:05:24 +0000

    Adding text input.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,49 @@
 import { Helmet } from "react-helmet";
+import { useState } from "react";
 
 export default function Home() {
+    const [ inputText, setInputText ] = useState("");
+
+    function handleInputText(event) {
+        setInputText(event.target.value);
+    }
+
+    function handleClearTextButton() {
+        setInputText("");
+    }
+
+    console.log(inputText, "<------- inputText")
+
     return (
         <div>
             <Helmet>
                 <meta name="robots" content="index, follow" />
                 <link rel="canonical" href="https://fontgenerator.co.uk/" />
                 <title>Font Generator • FontGenerator.co.uk</title>
-                <meta name="description" content="Copy and paste hundreds of fonts." />
+                <meta name="description" content="Copy and paste hundreds of fonts with our font generator." />
             </Helmet>
 
             <header>
                 <h1>Font Generator</h1>
-                <p>This is some text in the header tag.</p>
+                <p>Copy and paste hundreds of fonts with our font generator.</p>
             </header>
 
             <main>
-                <h2>Sub-heading</h2>
-                <p>This is some text in the main tag.</p>
+                <form>
+                    <label htmlFor="input-text">Input your text:</label>
+                    <textarea
+                        id="input-text"
+                        name="input-text"
+                        value={inputText}
+                        onChange={handleInputText}
+                    ></textarea>
+                    <div>
+                        <button
+                            type="button"
+                            onClick={handleClearTextButton}
+                        >Clear Text</button>
+                    </div>
+                </form>
             </main>
         </div>
     )
