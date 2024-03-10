6baa7bf12ca8f3e20b02ce00cb37f9a8f1629b54
Johnny Fong <johnnyfong@live.com>
2024-03-10 13:51:16 +0000

    Adding fonts.

--- src/content/fonts.js
+++ src/content/fonts.js
@@ -1,15 +1,17 @@
 export const allFonts = [
     {
-        name: "Original",
+        name: "Regular",
     },
 
     /*********************************************** TEMPLATE ***********************************************
     {
-        name: "xxxxxxxxxxxx",
-        openingCharacterWrapper: "xxxxxxxxxxxx",
-        closingCharacterWrapper: "xxxxxxxxxxxx",
-        openingTextWrapper: "xxxxxxxxxxxx",
-        closingTextWrapper: "xxxxxxxxxxxx",
+        name: "xxxxxxxxxxxxxxxxxx",
+        openingTextWrapper: "xxxxxxxxxxxxxxxxxx",
+        closingTextWrapper: "xxxxxxxxxxxxxxxxxx",
+        openingWordWrapper: "xxxxxxxxxxxxxxxxxx",
+        closingWordWrapper: "xxxxxxxxxxxxxxxxxx",
+        openingCharacterWrapper: "xxxxxxxxxxxxxxxxxx",
+        closingCharacterWrapper: "xxxxxxxxxxxxxxxxxx",    
         characters: {
             'a': 'a',
             'b': 'b',
@@ -65,94 +67,146 @@ export const allFonts = [
             'Z': 'Z',
         }
     },
+    {
+        name: "xxxxxxxxxxxxxxxxxx Text Wrapper",
+        tags: [
+            "xxxxxxxxxxxxxxxxxx",
+            "text",
+            "wrapper"
+        ],
+        openingTextWrapper: "xxxxxxxxxxxxxxxxxx ",
+        closingTextWrapper: " xxxxxxxxxxxxxxxxxx",
+    },
+    {
+        name: "xxxxxxxxxxxxxxxxxx Word Wrapper",
+        tags: [
+            "xxxxxxxxxxxxxxxxxx",
+            "word",
+            "wrapper"
+        ],
+        openingWordWrapper: "xxxxxxxxxxxxxxxxxx",
+        closingWordWrapper: "xxxxxxxxxxxxxxxxxx"
+    },
+    {
+        name: "xxxxxxxxxxxxxxxxxx Character Wrapper",
+        tags: [
+            "xxxxxxxxxxxxxxxxxx",
+            "character",
+            "wrapper"
+        ],
+        openingCharacterWrapper: "xxxxxxxxxxxxxxxxxx",
+        closingCharacterWrapper: "xxxxxxxxxxxxxxxxxx"
+    },
     *********************************************** TEMPLATE ***********************************************/
 
     {
-        name: "Smiley Face Text Wrapper",
-        openingTextWrapper: "☺",
-        closingTextWrapper: "☺",
+        name: "Black Heart Text Wrapper",
+        tags: [
+            "heart",
+            "text",
+            "wrapper"
+        ],
+        openingTextWrapper: "\u2764 ",
+        closingTextWrapper: " \u2764",
     },
     {
-        name: "Smiley Face Character Wrapper",
-        openingCharacterWrapper: "☺",
-        closingCharacterWrapper: "☺",
-    },
-    {
-        name: "Parentheses Character Wrapper",
+        name: "Brackets (Round) Character Wrapper",
+        tags: [
+            "brackets",
+            "character",
+            "wrapper"
+        ],
         openingCharacterWrapper: "(",
         closingCharacterWrapper: ")"
     },
     {
-        name: "Dice Character Wrapper",
-        openingCharacterWrapper: "⚀",
-        closingCharacterWrapper: "⚅"
+        name: "Brackets (Round) Word Wrapper",
+        tags: [
+            "heart",
+            "word",
+            "wrapper"
+        ],
+        openingWordWrapper: "(",
+        closingWordWrapper: ")"
     },
     {
-        name: "Handwriting 6 Dice Text Wrapper",
-        openingTextWrapper: "⚀ ⚁ ⚂ ⚃ ⚄ ⚅ ",
-        closingTextWrapper: " ⚅ ⚄ ⚃ ⚂ ⚁ ⚀",
-        characters: {
-            'a': '𝒶',
-            'b': '𝒷',
-            'c': '𝒸',
-            'd': '𝒹',
-            'e': '𝑒',
-            'f': '𝒻',
-            'g': '𝑔',
-            'h': '𝒽',
-            'i': '𝒾',
-            'j': '𝒿',
-            'k': '𝓀',
-            'l': '𝓁',
-            'm': '𝓂',
-            'n': '𝓃',
-            'o': '𝑜',
-            'p': '𝓅',
-            'q': '𝓆',
-            'r': '𝓇',
-            's': '𝓈',
-            't': '𝓉',
-            'u': '𝓊',
-            'v': '𝓋',
-            'w': '𝓌',
-            'x': '𝓍',
-            'y': '𝓎',
-            'z': '𝓏',
-            'A': '𝒜',
-            'B': 'ℬ',
-            'C': '𝒞',
-            'D': '𝒟',
-            'E': 'ℰ',
-            'F': 'ℱ',
-            'G': '𝒢',
-            'H': 'ℋ',
-            'I': 'ℐ',
-            'J': '𝒥',
-            'K': '𝒦',
-            'L': 'ℒ',
-            'M': 'ℳ',
-            'N': '𝒩',
-            'O': '𝒪',
-            'P': '𝒫',
-            'Q': '𝒬',
-            'R': 'ℛ',
-            'S': '𝒮',
-            'T': '𝒯',
-            'U': '𝒰',
-            'V': '𝒱',
-            'W': '𝒲',
-            'X': '𝒳',
-            'Y': '𝒴',
-            'Z': '𝒵',
-        }
+        name: "Light Smiley Face Text Wrapper",
+        tags: [
+            "smiley face",
+            "text",
+            "wrapper"
+        ],
+        openingTextWrapper: "☺",
+        closingTextWrapper: "☺",
     },
     {
-        name: "6 Dice",
+        name: "Dark Smiley Face Text Wrapper",
+        tags: [
+            "smiley face",
+            "text",
+            "wrapper"
+        ],
+        openingTextWrapper: "☻",
+        closingTextWrapper: "☻",
+    },
+    {
+        name: "6 Dice Text Wrapper",
+        tags: [
+            "dice",
+            "text",
+            "wrapper"
+        ],
         openingTextWrapper: "⚀ ⚁ ⚂ ⚃ ⚄ ⚅ ",
         closingTextWrapper: " ⚅ ⚄ ⚃ ⚂ ⚁ ⚀"
     },
     {
+        name: "Dice (1) Text Wrapper",
+        tags: [
+            "dice",
+            "text",
+            "wrapper"
+        ],
+        openingTextWrapper: "⚀ ",
+        closingTextWrapper: " ⚀"
+    },
+    {
+        name: "Dice (6) Text Wrapper",
+        tags: [
+            "dice",
+            "text",
+            "wrapper"
+        ],
+        openingTextWrapper: "⚅ ",
+        closingTextWrapper: " ⚅"
+    },
+    {
+        name: "Dice (1) Word Wrapper",
+        tags: [
+            "dice",
+            "word",
+            "wrapper"
+        ],
+        wordWrapper: "⚀"
+    },
+    {
+        name: "Dice (6) Word Wrapper",
+        tags: [
+            "dice",
+            "wrapper"
+        ],
+        wordWrapper: "⚅"
+    },
+    {
         name: "Handwriting",
+        // openingTextWrapper: "*",
+        // closingTextWrapper: "*",
+        // openingWordWrapper: "*",
+        // closingWordWrapper: "*",
+        // openingCharacterWrapper: "*",
+        // closingCharacterWrapper: "*",        
+        tags: [
+            "handwriting"
+        ],
         characters: {
             'a': '𝒶',
             'b': '𝒷',
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,17 @@ export default function Home() {
     const [ fonts, setFonts ] = useState([]);
     const [ copyButtonText, setCopyButtonText ] = useState("Copy");
     const [ buttonNumberClicked, setButtonNumberClicked ] = useState(null);
-    const [ isReverseTextCheckBoxChecked, setIsReverseTextCheckBoxChecked ] = useState(false);
+
+    const [ isUppercase, setIsUppercase ] = useState(false);
+    const [ isLowercase, setIsLowercase ] = useState(false);
+    const [ isReverseText, setIsReverseText ] = useState(false);
+    const [ isCapitalisation, setIsCapitalisation ] = useState(false);
+    const [ isCapitalisationOdd, setIsCapitalisationOdd ] = useState(false);
+    const [ isCapitalisationEven, setIsCapitalisationEven ] = useState(false);
+    const [ isCapitalisationRandom, setIsCapitalisationRandom ] = useState(false);
+    const [ isCamelCase, setIsCamelCase ] = useState(false);
+    const [ isPascalCase, setIsPascalCase ] = useState(false);
+    const [ isURLFriendly, setIsURLFriendly ] = useState(false);
 
     useEffect(() => {
         const fontsToDisplay = [];
@@ -25,19 +35,73 @@ export default function Home() {
                 text = inputText;
             }
 
-            let textReversed;
-            if (isReverseTextCheckBoxChecked) {
-                textReversed = text.split('').reverse().join('');
-            } else {
-                textReversed = text;
+            if (isUppercase) {
+                text = utils.toUppercase(text);
             }
 
-            const convertedText = utils.convertText(textReversed, font.characters, font.openingCharacterWrapper, font.closingCharacterWrapper, font.openingTextWrapper, font.closingTextWrapper);
+            if (isLowercase) {
+                text = utils.toLowercase(text);
+            }
+
+            if (isReverseText) {
+                text = utils.reverseText(text);
+            }
+
+            if (isCapitalisation) {
+                text = utils.toCapitalisation(text);
+            }
+
+            if (isCapitalisationOdd) {
+                text = utils.toCapitalisationOdd(text);
+            }
+
+            if (isCapitalisationEven) {
+                text = utils.toCapitalisationEven(text);
+            }
+
+            if (isCapitalisationRandom) {
+                text = utils.toCapitalisationRandom(text);
+            }
+
+            if (isCamelCase) {
+                text = utils.toCamelCase(text);
+            }
+
+            if (isPascalCase) {
+                text = utils.toPascalCase(text);
+            }
+
+            if (isURLFriendly) {
+                text = utils.toURLFriendly(text);
+            }
+
+            const convertedText = utils.convertText(
+                text,
+                font.characters,
+                font.openingTextWrapper,
+                font.closingTextWrapper,
+                font.openingWordWrapper,
+                font.closingWordWrapper,
+                font.openingCharacterWrapper,
+                font.closingCharacterWrapper
+            );
             fontCopy.text = convertedText;            
             fontsToDisplay.push(fontCopy);
         })
         setFonts(fontsToDisplay);
-    }, [inputText, isReverseTextCheckBoxChecked])
+    }, [
+        inputText,
+        isUppercase,
+        isLowercase,
+        isReverseText,
+        isCapitalisation,
+        isCapitalisationOdd,
+        isCapitalisationEven,
+        isCapitalisationRandom,
+        isCamelCase,
+        isPascalCase,
+        isURLFriendly
+    ])
 
     function handleInputText(event) {
         setInputText(event.target.value);
@@ -51,11 +115,47 @@ export default function Home() {
         setButtonNumberClicked(buttonNumber);
         navigator.clipboard.writeText(text);
         setCopyButtonText("Copied");
-        setTimeout(() => setCopyButtonText("Copy"), 2000);
+        setTimeout(() => setCopyButtonText("Copy"), 3000);
     };
 
-    function handleReverseTextCheckBox() {
-        setIsReverseTextCheckBoxChecked((currentTextReversedValue) => !currentTextReversedValue);
+    function handleUppercase() {
+        setIsUppercase((currentUppercaseValue) => !currentUppercaseValue);
+    }
+
+    function handleLowercase() {
+        setIsLowercase((currentLowercaseValue) => !currentLowercaseValue);
+    }
+
+    function handleReverseText() {
+        setIsReverseText((currentTextReversedValue) => !currentTextReversedValue);
+    }
+
+    function handleCapitalisation() {
+        setIsCapitalisation((currentCapitalisationValue) => !currentCapitalisationValue);
+    }
+
+    function handleCapitalisationOdd() {
+        setIsCapitalisationOdd((currentCapitalisationOddValue) => !currentCapitalisationOddValue);
+    }
+
+    function handleCapitalisationEven() {
+        setIsCapitalisationEven((currentCapitalisationEvenValue) => !currentCapitalisationEvenValue);
+    }
+
+    function handleCapitalisationRandom() {
+        setIsCapitalisationRandom((currentCapitalisationRandomValue) => !currentCapitalisationRandomValue);
+    }
+
+    function handleCamelCase() {
+        setIsCamelCase((currentCamelCaseValue) => !currentCamelCaseValue);
+    }
+
+    function handlePascalCase() {
+        setIsPascalCase((currentPascalCaseValue) => !currentPascalCaseValue);
+    }
+
+    function handleURLFriendly() {
+        setIsURLFriendly((currentURLFriendlyValue) => !currentURLFriendlyValue);
     }
 
     if (!fonts) {
@@ -80,7 +180,6 @@ export default function Home() {
                 <section>
                     <form>
                         <label htmlFor="input-text">Input your text:</label>
-
                         <textarea
                             id="input-text"
                             name="input-text"
@@ -89,18 +188,212 @@ export default function Home() {
                             placeholder={placeholderText}
                         ></textarea>
 
-                        <div>
-                            <input
-                                type="checkbox"
-                                id="reverse-text"
-                                name="reverse-text"
-                                value="Reverse Text"
-                                onChange={handleReverseTextCheckBox}
-                                checked={isReverseTextCheckBoxChecked}
-                            />
-                            <label htmlFor="vehicle1">Reverse Text</label>
-                        </div>
-                        
+                        <fieldset>
+                            <legend>Options:</legend>
+                            <div id="font-options">
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="reverse-text"
+                                        name="reverse-text"
+                                        value="Reverse Text"
+                                        onChange={handleReverseText}
+                                        checked={isReverseText}
+                                    />
+                                    <label htmlFor="reverse-text">Reverse Text</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="uppercase"
+                                        name="uppercase"
+                                        value="Uppercase"
+                                        onChange={handleUppercase}
+                                        checked={isUppercase}
+                                        disabled={
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="uppercase">Uppercase</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="lowercase"
+                                        name="lowercase"
+                                        value="Lowercase"
+                                        onChange={handleLowercase}
+                                        checked={isLowercase}
+                                        disabled={
+                                            isUppercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="lowercase">Lowercase</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="capitalisation"
+                                        name="capitalisation"
+                                        value="Capitalisation"
+                                        onChange={handleCapitalisation}
+                                        checked={isCapitalisation}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="capitalisation">Capitalisation</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="capitalisation-odd"
+                                        name="capitalisation-odd"
+                                        value="Capitalisation Odd"
+                                        onChange={handleCapitalisationOdd}
+                                        checked={isCapitalisationOdd}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="capitalisation-odd">Capitalisation (Odd)</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="capitalisation-even"
+                                        name="capitalisation-even"
+                                        value="Capitalisation Even"
+                                        onChange={handleCapitalisationEven}
+                                        checked={isCapitalisationEven}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="capitalisation-even">Capitalisation (Even)</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="capitalisation-random"
+                                        name="capitalisation-random"
+                                        value="Capitalisation Random"
+                                        onChange={handleCapitalisationRandom}
+                                        checked={isCapitalisationRandom}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCamelCase ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="capitalisation-random">Capitalisation (Random)</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="camel-case"
+                                        name="camel-case"
+                                        value="Camel Case"
+                                        onChange={handleCamelCase}
+                                        checked={isCamelCase}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isPascalCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="camel-case">Camel Case</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="pascal-case"
+                                        name="pascal-case"
+                                        value="pascal Case"
+                                        onChange={handlePascalCase}
+                                        checked={isPascalCase}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isURLFriendly
+                                        }
+                                    />
+                                    <label htmlFor="pascal-case">Pascal Case</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="url-friendly"
+                                        name="url-friendly"
+                                        value="URL Friendly"
+                                        onChange={handleURLFriendly}
+                                        checked={isURLFriendly}
+                                        disabled={
+                                            isUppercase ||
+                                            isLowercase ||
+                                            isCapitalisation ||
+                                            isCapitalisationOdd ||
+                                            isCapitalisationEven ||
+                                            isCapitalisationRandom ||
+                                            isCamelCase ||
+                                            isPascalCase
+                                        }
+                                    />
+                                    <label htmlFor="url-friendly">URL Friendly</label>
+                                </div>
+                            </div>
+                        </fieldset>
+
                         <div>
                             <button
                                 type="button"
